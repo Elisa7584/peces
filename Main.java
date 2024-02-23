c72ea5b6d67bb43ef130a2434fa29a3c712b4614
Elisa Ortega Girard <145747076+Elisa7584@users.noreply.github.com>
2024-02-23 10:16:54 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,4 +1,8 @@
-public static void main(String[] args) {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
         // Crear una lista de CINCO estantes
         List<Estanteria> estanterias = new ArrayList<>();
         for (int i = 0; i < 5; i++) {
@@ -8,20 +12,21 @@ public static void main(String[] args) {
         // Rellenar los estantes con objetos
         double[] pesosObjetos = {10, 20, 5, 15, 30, 25};
         int estanteriaActual = 0;
+        boolean objetoAñadido;
 
         for (double peso : pesosObjetos) {
-            while (estanteriaActual < estanterias.size()) {
+            objetoAñadido = false;
+            while (estanteriaActual < estanterias.size() && !objetoAñadido) {
                 if (estanterias.get(estanteriaActual).añadirObjeto(peso)) {
                     System.out.println("Se ha añadido un objeto de " + peso + "kg a la estantería " + estanteriaActual);
-                    break;
+                    objetoAñadido = true;
                 } else {
                     System.out.println("El objeto de " + peso + "kg no cabe en la estantería " + estanteriaActual + ", se pasa a la siguiente.");
                     estanteriaActual++;
                 }
             }
-            if (estanteriaActual >= estanterias.size()) {
+            if (!objetoAñadido) {
                 System.out.println("No hay más estanterías disponibles para el objeto de " + peso + "kg");
-                break;
             }
         }
 
@@ -31,3 +36,4 @@ public static void main(String[] args) {
             System.out.println("Estantería " + i + ": " + estanterias.get(i));
         }
     }
+}
