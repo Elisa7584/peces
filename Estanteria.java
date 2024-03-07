388165ef1339a9387f4f86643866e6a88ccd2322
Elisa7584 <elisaortega112.alu@stemgranada.com>
2024-03-07 11:30:30 +0100

    .

---
+++ Estanteria.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Estanteria {
+    private double pesoMaximo;
+    private List<Double> pesosObjetos;
+
+    // Constructor por defecto
+    public Estanteria() {
+        this.pesoMaximo = 35;
+        this.pesosObjetos = new ArrayList<>();
+    }
+
+    // Constructor con parámetros
+    public Estanteria(double pesoMaximo, List<Double> pesosObjetos) {
+        this.pesoMaximo = pesoMaximo;
+        this.pesosObjetos = pesosObjetos;
+    }
+
+    // Método para calcular el peso total
+    public double pesoTotal() {
+        double pesoTotal = 0;
+        for (Double peso : pesosObjetos) {
+            pesoTotal += peso;
+        }
+        return pesoTotal;
+    }
+
+    // Método para añadir un objeto si es posible, retorna True si es posible, False si no
+    public boolean añadirObjeto(double peso) {
+        if (pesoTotal() + peso <= pesoMaximo) {
+            pesosObjetos.add(peso);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "Estanteria{" +
+                "pesoMaximo=" + pesoMaximo +
+                ", objetosSoportados=" + pesosObjetos.size() +
+                '}';
+    }
+}
